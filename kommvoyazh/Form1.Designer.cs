b604aa747fc02ebe1cd3508322aa18db1331e35e
ВиноградовРА@ngknn.local <@ngknn.local>
2021-05-25 16:25:43 +0300

    Добавьте файлы проекта.

---
+++ kommvoyazh/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace kommvoyazh
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vvodkolputei = new System.Windows.Forms.TextBox();
+            this.sohrrazm = new System.Windows.Forms.Button();
+            this.sms = new System.Windows.Forms.Label();
+            this.mas = new System.Windows.Forms.DataGridView();
+            this.Vvodmas = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // vvodkolputei
+            // 
+            this.vvodkolputei.Location = new System.Drawing.Point(199, 99);
+            this.vvodkolputei.Name = "vvodkolputei";
+            this.vvodkolputei.Size = new System.Drawing.Size(148, 23);
+            this.vvodkolputei.TabIndex = 0;
+            this.vvodkolputei.TextChanged += new System.EventHandler(this.vvodkolputei_TextChanged);
+            // 
+            // sohrrazm
+            // 
+            this.sohrrazm.ForeColor = System.Drawing.Color.Black;
+            this.sohrrazm.Location = new System.Drawing.Point(225, 166);
+            this.sohrrazm.Name = "sohrrazm";
+            this.sohrrazm.Size = new System.Drawing.Size(89, 29);
+            this.sohrrazm.TabIndex = 1;
+            this.sohrrazm.Text = "button1";
+            this.sohrrazm.UseVisualStyleBackColor = true;
+            this.sohrrazm.Click += new System.EventHandler(this.sohrrazm_Click);
+            // 
+            // sms
+            // 
+            this.sms.AutoSize = true;
+            this.sms.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.sms.Location = new System.Drawing.Point(12, 292);
+            this.sms.Name = "sms";
+            this.sms.Size = new System.Drawing.Size(0, 15);
+            this.sms.TabIndex = 2;
+            // 
+            // mas
+            // 
+            this.mas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mas.Location = new System.Drawing.Point(12, 12);
+            this.mas.Name = "mas";
+            this.mas.RowTemplate.Height = 25;
+            this.mas.Size = new System.Drawing.Size(566, 263);
+            this.mas.TabIndex = 3;
+            this.mas.Visible = false;
+            // 
+            // Vvodmas
+            // 
+            this.Vvodmas.ForeColor = System.Drawing.Color.Black;
+            this.Vvodmas.Location = new System.Drawing.Point(225, 274);
+            this.Vvodmas.Name = "Vvodmas";
+            this.Vvodmas.Size = new System.Drawing.Size(99, 36);
+            this.Vvodmas.TabIndex = 4;
+            this.Vvodmas.Text = "Сохранить";
+            this.Vvodmas.UseVisualStyleBackColor = true;
+            this.Vvodmas.Visible = false;
+            this.Vvodmas.Click += new System.EventHandler(this.Vvodmas_Click);
+            // 
+            // next
+            // 
+            this.next.ForeColor = System.Drawing.Color.Black;
+            this.next.Location = new System.Drawing.Point(503, 281);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(75, 23);
+            this.next.TabIndex = 5;
+            this.next.Text = "Далее";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Visible = false;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 316);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.Vvodmas);
+            this.Controls.Add(this.mas);
+            this.Controls.Add(this.sms);
+            this.Controls.Add(this.sohrrazm);
+            this.Controls.Add(this.vvodkolputei);
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox vvodkolputei;
+        private System.Windows.Forms.Button sohrrazm;
+        private System.Windows.Forms.Label sms;
+        private System.Windows.Forms.DataGridView mas;
+        private System.Windows.Forms.Button Vvodmas;
+        private System.Windows.Forms.Button next;
+    }
+}
+
---
+++ kommvoyazh/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kommvoyazh
+{
+    public partial class Form1 : Form
+    {
+        int uzli, otv;
+        int[,] time;
+        int[] f;
+        string[] puti;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sohrrazm_Click(object sender, EventArgs e)
+        {
+            uzli = vvodint(true, false, 0);
+            if (uzli > 0)
+            {
+                time = new int[uzli, uzli];
+                sohrrazm.Visible = false;
+                vvodkolputei.Visible = false;
+                mas.Visible = true;
+                Vvodmas.Visible = true;
+                for (int i = 0; i < uzli; i++)
+                    mas.Columns.Add("col" + i, "" + (i + 1));
+                mas.Rows.Add(uzli);
+            }
+
+        }
+
+        private void vvodkolputei_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Vvodmas_Click(object sender, EventArgs e)
+        {
+            vvodint();
+        }
+        public void vvodint()
+        {
+
+            try
+            {
+                int sch = 0;
+                for (int i1 = 0; i1 < uzli; i1++)
+                {
+                    for (int i2 = 0; i2 < uzli; i2++)
+                    {
+                        if (i1 == i2)
+                        {
+
+                            time[i1, i2] = 0;
+                            if (sch == 0)
+                                MessageBox.Show("Путь из точки в саму себя\nможет быть равен только нулю!\nЗаменили их на 0!", "Внимание!");
+                            sch++;
+                        }
+                        else
+                        {
+                            time[i1, i2] = Convert.ToInt32(mas.Rows[i1].Cells[i2].Value);
+                            if (time[i1, i2] <= 0)
+                                throw new Exception("Меньше или равно нулю");
+                        }
+                    }
+                }
+                for (int i1 = 0; i1 < uzli; i1++)
+                {
+                    for (int i2 = 0; i2 < uzli; i2++)
+                    {
+                        mas.Rows[i1].Cells[i2].Value = time[i1, i2];
+                    }
+                }
+                MessageBox.Show("Если Вас всё устраивает - нажмите на кнопку \"Далее\"\nВ противном случае измените данные и сохраните снова!");
+            }
+            catch (Exception ex)
+            {
+                switch (ex.Message)
+                {
+                    case "Меньше или равно нулю":
+                        MessageBox.Show("ОШИБКА! Число должно быть больше нуля!", "Ошибка");
+                        break;
+                    default:
+                        MessageBox.Show("ОШИБКА! Введите целые числа во все ячейка","Ошибка");
+                        break;
+                }
+            }
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            mas.Visible = !mas.Visible;
+            Vvodmas.Visible = !Vvodmas.Visible;
+            next.Visible = !next.Visible;
+        }
+
+        public int vvodint(bool bolshenulya, bool vopros, int kolvarotv)
+        {
+            int vvod;
+            try
+            {
+                vvod = Convert.ToInt32(vvodkolputei.Text);
+                if (bolshenulya)
+                {
+                    if (vvod <= 0)
+                        throw new Exception("Меньше или равно нулю");
+                }
+                if (vopros)
+                {
+                    if (vvod < 0 || vvod >= kolvarotv)
+                        throw new Exception("Ответ не совпадает с вариантом");
+                }
+            }
+            catch (Exception ex)
+            {
+                switch (ex.Message)
+                {
+                    case "Меньше или равно нулю":
+                        MessageBox.Show("ОШИБКА! Число должно быть больше нуля!","Ошибка");
+                        break;
+                    case "Ответ не совпадает с вариантом":
+                        MessageBox.Show("ОШИБКА! Такого варианта не существует!","Ошибка");
+                        break;
+                    default:
+                        MessageBox.Show("ОШИБКА! Введите целое число!", "Ошибка");
+                        break;
+                }
+                return 0;
+            }
+            return vvod;
+        }
+    }
+}
